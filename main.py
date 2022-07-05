0cd9c252af741bf16d778e57e520958a268cf2f5
Massimiliano Falsini <falsinimassimiliano@gmail.com>
2022-07-05 13:43:03 +0200

    refactor

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # https://developers.google.com/gmail/imap
 from fastapi import FastAPI
 
-from oauth2 import (authorizing_token, creating_authentication_string,
-                    generate_permission_url, imap_authentication, smtp_authentication, refresh_token_func)
+from oauth2 import (generate_permission_url,
+                    generate_token_authorization, generate_o_auth_2_string, imap_authentication, smtp_authentication, refresh_token_func)
 
 # Add your client id and client secret
 CLIENT_ID: str = ''
@@ -18,11 +18,12 @@ async def get_url() -> dict:
     return {'url': url}
 
 
-# http://localhost:8080/get_token?token=[]
-@app.get('/get_token')
-async def get_token(token: str) -> dict:
-    refresh_token, access_token, access_token_expiration_seconds = authorizing_token(
+# http://localhost:8080/authorize_token?token=[]
+@app.get('/authorize_token')
+async def authorize_token(token: str) -> dict:
+    refresh_token, access_token, access_token_expiration_seconds = generate_token_authorization(
         CLIENT_ID, CLIENT_SECRET, token)
+
     return {
         'refresh_token': refresh_token,
         'access_token': access_token,
@@ -33,7 +34,7 @@ async def get_token(token: str) -> dict:
 # http://localhost:8080/get_auth_str?access_token=[]&email=[]
 @app.get('/get_auth_str')
 async def get_auth_str(access_token: str, email: str) -> dict:
-    o_auth2_argument: str = creating_authentication_string(
+    o_auth2_argument: str = generate_o_auth_2_string(
         access_token, email)
     return {'o_auth2_argument': o_auth2_argument}
 
@@ -52,11 +53,11 @@ async def smtp(access_token: str, email: str) -> dict:
     return{'': ''}
 
 
-# http://localhost:8080/refresh_token?refresh_token=[]
-@app.get('/refresh_token_func')
-async def refresh_token_func(refresh_token: str) -> dict:
-    refresh_token, access_token, access_token_expiration_seconds = refresh_token_func(
-        CLIENT_ID, CLIENT_SECRET, refresh_token)
+# http://localhost:8080/refresh_token?refresh_token_req=[]
+@app.get('/refresh_token')
+async def refresh_token(refresh_token_req: str) -> dict:
+    access_token, access_token_expiration_seconds = refresh_token_func(
+        CLIENT_ID, CLIENT_SECRET, refresh_token_req)
     return {
         'access_token': access_token,
         'access_token_expiration_seconds': access_token_expiration_seconds,
--- oauth2.py
+++ oauth2.py
@@ -12,125 +12,74 @@ GOOGLE_ACCOUNTS_BASE_URL: str = 'https://accounts.google.com'
 REDIRECT_URI: str = 'urn:ietf:wg:oauth:2.0:oob'
 
 
-# **************************************************
-def __accounts_url(command: str) -> str:
-    return '%s/%s' % (GOOGLE_ACCOUNTS_BASE_URL, command)
-
-
-def __url_escape(text: str) -> str:
-    return urllib.parse.quote(text, safe='~-._')
-
-
-def __format_url_params(params: dict) -> str:
-    param_fragments = []
-    for param in sorted(params.items(), key=lambda x: x[0]):
-        param_fragments.append('%s=%s' % (param[0], __url_escape(param[1])))
-    return '&'.join(param_fragments)
-
-
-def __generate_permission_url(client_id: str) -> str:
-    params = {}
+def generate_permission_url(client_id: str) -> str:
+    params: dict = {}
     params['client_id'] = client_id
     params['redirect_uri'] = REDIRECT_URI
     params['scope'] = 'https://mail.google.com/'
     params['response_type'] = 'code'
-    return '%s?%s' % (__accounts_url('o/oauth2/auth'),
-                      __format_url_params(params))
-# **************************************************
+
+    param_fragments: list = []
+    for param in sorted(params.items(), key=lambda x: x[0]):
+        param_fragments.append('%s=%s' % (
+            param[0], urllib.parse.quote(param[1], safe='~-._')))
+    formated_url_params: str = '&'.join(param_fragments)
+
+    return '%s?%s' % ('%s/%s' % (GOOGLE_ACCOUNTS_BASE_URL, 'o/oauth2/auth'), formated_url_params)
 
 
-# **************************************************
-def __accounts_url(command: str) -> str:
-    return '%s/%s' % (GOOGLE_ACCOUNTS_BASE_URL, command)
-
-
-def __authorize_tokens(client_id: str, client_secret: str, authorization_code: str) -> dict:
-    params = {}
+def generate_token_authorization(client_id: str, client_secret: str, authorization_code: str) -> Tuple[str, str, int]:
+    params: dict = {}
     params['client_id'] = client_id
     params['client_secret'] = client_secret
     params['code'] = authorization_code
     params['redirect_uri'] = REDIRECT_URI
     params['grant_type'] = 'authorization_code'
-    request_url = __accounts_url('o/oauth2/token')
+    request_url: str = '%s/%s' % (GOOGLE_ACCOUNTS_BASE_URL, 'o/oauth2/token')
 
-    response = urlopen(request_url, urllib.parse.urlencode(
+    response: dict = urlopen(request_url, urllib.parse.urlencode(
         params).encode('UTF-8')).read().decode("utf-8")
-    return json.loads(response)
-# **************************************************
+    json_response: dict = json.loads(response)
+    return json_response['refresh_token'], json_response['access_token'], json_response['expires_in']
 
 
-# **************************************************
-def __generate_o_auth_2_string(email: str, access_token: str, base64_encode: bool = True) -> str:
-    auth_string = 'user=%s\1auth=Bearer %s\1\1' % (email, access_token)
+def generate_o_auth_2_string(email: str, access_token: str, base64_encode: bool = True) -> str:
+    auth_string: str = 'user=%s\1auth=Bearer %s\1\1' % (email, access_token)
     if base64_encode:
-        auth_string = base64.b64encode(bytes(auth_string, encoding='UTF-8'))
+        auth_string: str = base64.b64encode(
+            bytes(auth_string, encoding='UTF-8'))
     return auth_string
-# **************************************************
 
 
-# **************************************************
-def __test_imap_authentication(user: str, auth_string: str):
-    # print
-    imap_conn = imaplib.IMAP4_SSL('imap.gmail.com')
+def imap_authentication(email: str, access_token: str) -> None:
+    o_auth2_argument: str = generate_o_auth_2_string(
+        email, access_token, False)
+    imap_conn: imaplib.IMAP4_SSL = imaplib.IMAP4_SSL('imap.gmail.com')
     imap_conn.debug = 4
-    imap_conn.authenticate('XOAUTH2', lambda x: auth_string)
+    imap_conn.authenticate('XOAUTH2', lambda x: o_auth2_argument)
     imap_conn.select('INBOX')
-# **************************************************
 
 
-# **************************************************
-def __test_smtp_authentication(user: str, auth_string: str):
-    print
-    smtp_conn = smtplib.SMTP('smtp.gmail.com', 587)
+def smtp_authentication(email: str, access_token: str) -> None:
+    o_auth2_argument: str = generate_o_auth_2_string(
+        email, access_token, False)
+    smtp_conn: smtplib.SMTP = smtplib.SMTP('smtp.gmail.com', 587)
     smtp_conn.set_debuglevel(True)
     smtp_conn.ehlo('test')
     smtp_conn.starttls()
-    smtp_conn.docmd('AUTH', 'XOAUTH2 ' + base64.b64encode(auth_string))
-# **************************************************
+    smtp_conn.docmd('AUTH', 'XOAUTH2 ' + ''.join(chr(x)
+                    for x in bytearray(base64.b64encode(bytes(o_auth2_argument, encoding='UTF-8')))))
 
 
-# **************************************************
-def __accounts_url(command):
-    return '%s/%s' % (GOOGLE_ACCOUNTS_BASE_URL, command)
-
-
-def __refresh_token(client_id: str, client_secret: str, refresh_token: str) -> dict:
-    params = {}
+def refresh_token_func(client_id: str, client_secret: str, refresh_token: str) -> Tuple[str, int]:
+    params: dict = {}
     params['client_id'] = client_id
     params['client_secret'] = client_secret
     params['refresh_token'] = refresh_token
     params['grant_type'] = 'refresh_token'
-    request_url = __accounts_url('o/oauth2/token')
+    request_url = '%s/%s' % (GOOGLE_ACCOUNTS_BASE_URL, 'o/oauth2/token')
 
-    response = urllib.urlopen(request_url, urllib.urlencode(
+    response: dict = urlopen(request_url, urllib.parse.urlencode(
         params).encode('UTF-8')).read().decode("utf-8")
-    return json.loads(response)
-# **************************************************
-
-
-def generate_permission_url(client_id: str) -> str:
-    return __generate_permission_url(client_id)
-
-
-def authorizing_token(client_id: str, client_secret: str, token: str) -> Tuple[str, str, int]:
-    json: dict = __authorize_tokens(client_id, client_secret, token)
-    return json['refresh_token'], json['access_token'], json['expires_in']
-
-
-def creating_authentication_string(access_token: str, email: str) -> str:
-    return __generate_o_auth_2_string(email, access_token)
-
-
-def imap_authentication(email: str, access_token: str):
-    __test_imap_authentication(
-        email, __generate_o_auth_2_string(email, access_token, False))
-
-
-def smtp_authentication(email: str, access_token: str):
-    __test_smtp_authentication(
-        email, __generate_o_auth_2_string(email, access_token, False))
-
-
-def refresh_token_func(client_id: str, client_secret: str, refresh_token: str) -> Tuple[str, int]:
-    json: dict = __refresh_token(client_id, client_secret,  refresh_token)
-    return json['access_token'], json['expires_in']
+    json_response: dict = json.loads(response)
+    return json_response['access_token'], json_response['expires_in']
